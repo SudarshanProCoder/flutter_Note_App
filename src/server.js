d02a4f4b8efd16afe94a488ea5d5e1e7dac548ff
SudarshanProCoder <sudarshandate21@gmail.com>
2024-04-26 01:22:14 +0530

    done

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const Note = require("./models/Note");
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/notesdb").then(function(){
+
+    app.get("/", function(req, res){
+        const response = {statuscode: res.statusCode, message: "API Works!"};
+        res.send(response);
+
+    });
+
+    const noteRouter = require('./routes/Note');
+
+    app.use("/notes", noteRouter);
+
+   
+
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function(){
+    console.log("server started at PORT : " + PORT);
+});
